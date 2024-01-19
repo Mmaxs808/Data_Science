b6dabca71bbb0e51913055be0c29b14ed1411262
Jacob Shigeta <71992776+Mmaxs808@users.noreply.github.com>
2024-01-19 12:05:30 -0700

    Add files via upload

---
+++ testPackage/R/testcode.R
@@ -0,0 +1,7 @@
+#This is a test file to test a function and a command
+
+hello_world <- function(){
+  print("Hello World!")
+}
+
+print("Should be on a new line")
---
+++ testPackage/R/testfile.R
@@ -0,0 +1,2 @@
+#This is a test file in my test package
+#to see if it actually pushed into GitHub
