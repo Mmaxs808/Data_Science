876a7454e1437984fb9fbe4f6f175cc17142541d
Jacob Shigeta <71992776+Mmaxs808@users.noreply.github.com>
2024-01-24 11:55:49 -0700

    Delete testPackage directory
    Misunderstood Assignment. Other directory is the one with the package I will be using

--- testPackage/R/testcode.R
+++
@@ -1,7 +0,0 @@
-#This is a test file to test a function and a command
-
-hello_world <- function(){
-  print("Hello World!")
-}
-
-print("Should be on a new line")
--- testPackage/R/testfile.R
+++
@@ -1,2 +0,0 @@
-#This is a test file in my test package
-#to see if it actually pushed into GitHub
