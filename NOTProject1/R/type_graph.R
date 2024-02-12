0c40525a1d20ef9b2444d81245cef52ad83f39ef
Jacob Shigeta <71992776+Mmaxs808@users.noreply.github.com>
2024-02-12 12:00:24 -0700

    Reformatted Folder

--- NOTProject1/R/type_graph.R
+++
@@ -1,9 +0,0 @@
-#Making the Type graph
-type_graph <- ggplot(typeData, aes(x=reorder(Primary_Type, Primary_Type, function(x)-length(x)), fill=Primary_Type)) +
-  geom_bar(color = "black") +
-  xlab("Primary Type") +
-  ylab("Number of Pokemon") +
-  theme(axis.text.x = element_text(angle=90),
-        legend.position = "none")
-
-type_graph
